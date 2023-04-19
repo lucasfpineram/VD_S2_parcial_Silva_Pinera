d3de669b6bb334a65a545cc1ff89c66451da71f5
luchosd02 <luchosilvadavidov@gmail.com>
2023-04-19 10:57:09 -0300

    nfinlkf

--- graficos_definitivos/mean_top3_cumplidas/script.js
+++ graficos_definitivos/mean_top3_cumplidas/script.js
@@ -1,30 +1,33 @@
-d3.dsv(';','147_desratizacion.csv', d3.autoType).then(data => {
+d3.dsv(';', '147_desratizacion.csv', d3.autoType).then(data => {
   let chart = Plot.plot({
-  marks: [
-    Plot.rectX(
-      data.filter(item => item.domicilio_barrio == 'PALERMO' || item.domicilio_barrio == 'VILLA URQUIZA' || item.domicilio_barrio == 'CABALLITO'),
-      Plot.binY(
-        { x: "proportion" },
-        {
-          x: "proporcion",
-          y: "date",
-          fill: "red",
-          fillOpacity: 0.4,
-          thresholds: d3.utcMonth
-        }
+    marks: [
+      Plot.rectX(
+        data.filter(item => item.domicilio_barrio == 'PALERMO' || item.domicilio_barrio == 'VILLA URQUIZA' || item.domicilio_barrio == 'CABALLITO'),
+        Plot.binY(
+          { x: "proportion" },
+          {
+            x: "estado_del_contacto",
+            y: "date",
+            fill: "red",
+            fillOpacity: 0.4,
+            thresholds: d3.utcMonth
+          }
+        )
       )
-    )
-  ],
-  marginLeft: 100,
-  width: 666,
-  y: { reverse: true }, 
-  color: {
-    legend: true
-  }
+    ],
+    marginLeft: 100,
+    width: 666,
+    y: { reverse: true },
+    color: {
+      legend: true
+    }
+  });
+
+  d3.select('#chart').node().appendChild(chart);
+}).catch(error => {
+  console.log(error);
 });
 
-d3.select('#chart').append(() => chart)
-})
 
 
 
